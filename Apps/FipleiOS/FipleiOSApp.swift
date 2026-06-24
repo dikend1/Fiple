d9d24fe9b352586cd44401151912b215e71a9fbd
Dias Maksatov <diasmaksatov78@gmail.com>
2026-06-24 11:12:55 +0500

    Lock iOS app to light color scheme for correct text rendering

--- Apps/FipleiOS/FipleiOSApp.swift
+++ Apps/FipleiOS/FipleiOSApp.swift
@@ -7,6 +7,9 @@ struct FipleiOSApp: App {
     var body: some Scene {
         WindowGroup {
             RootView(controller: controller)
+                .preferredColorScheme(.light) // app uses a fixed light palette; lock the
+                                              // scheme so dark-mode devices don't render
+                                              // adaptive text (section headers, tab labels) white.
                 .task { controller.begin() }
         }
     }
